8a7ef925ee7c81ecaacfd949522ef5fe43a93d1e
TBrias <tommy.brias@gmail.com>
2023-10-26 13:22:02 +0200

    configure scrapy spider

--- predictice_scrapy/predictice_scrapy/settings.py
+++ predictice_scrapy/predictice_scrapy/settings.py
@@ -15,6 +15,7 @@ NEWSPIDER_MODULE = "predictice_scrapy.spiders"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "predictice_scrapy (+http://www.yourdomain.com)"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
--- predictice_scrapy/predictice_scrapy/spiders/legifrance_spider.py
+++ predictice_scrapy/predictice_scrapy/spiders/legifrance_spider.py
@@ -1,45 +1,53 @@
 import scrapy
 import logging
 from scrapy.utils.log import configure_logging 
+import pandas as pd
 
 logger = logging.getLogger("mylog")
 
-class LegifranceSpider(scrapy.Spider):
-    configure_logging(install_root_handler=False)
-    logging.basicConfig(
-        filename='log.txt',
-        format='%(levelname)s: %(message)s',
-        level=logging.INFO
-    )
 
-    logger.warn("legifrance_spider script is starting")
+
+class LegifranceSpider(scrapy.Spider):
+    logger.warn("\n **** legifrance_spider script is starting **** \n")
 
     name = "legifrance_spider"
     allowed_domains = ["legifrance.gouv.fr"]
     start_urls = ["https://www.legifrance.gouv.fr/search/juri?tab_selection=juri&searchField=ALL&query=*&searchType=ALL&dateDecision=01%2F06%2F2022+%3E+30%2F06%2F2022&typePagination=DEFAULT&sortValue=DATE_DESC&pageSize=10&page=1&tab_selection=juri#juri"]
 
     def parse(self, response):
-        logger.warning("Parse is called")
-        links = response.css("article.result-item::attr(href)").getall()
-        logger.warning("AAAAAAAAAAAAAAA   AAAAAAAAAAAAAAAAAAAAAa")
-        logger.warning("**** links ****: ", links)
+        self.data = []
+        logger.warning("\n Parse is called \n")
+        logger.warning("Parse is called on URL: " + response.url)
+        links = response.css("article.result-item > h2 a::attr(href)").getall()
+        logger.warning("\n**** links ****:  %s", links)
 
         for link in links:
-            logger.warning("**** link ****: ", link)
+            logger.warning("\n**** link ****:  %s", link)
             yield response.follow(link, callback=self.parse_detail)
 
     def parse_detail(self, response):
-        # Ici, vous pouvez extraire les détails de la page individuelle
-        titre = response.css("h1.titre-texte::text").get()
-        juridiction = response.css(".juridiction::text").get()
-        date = response.css(".date-decision::text").get()
-        numero_decision = response.css(".numero-decision::text").get()
-        texte_document = response.css(".texte-document::text").get()
+        logger.warning("\n parse_detail is called \n")
+        
+        titre = response.css("h1.main-title::text").get()
+        logger.warning("**** titre ****: " + titre)
 
-        yield {
+        metadata = titre.split(',')
+        #texte_document = response.css("div.content-page > div::text").getall()
+        texte_document = "\n".join(response.xpath('//div[@class="content-page"]//div//text()').extract())
+        
+
+        item = {
             "titre": titre,
-            "juridiction": juridiction,
-            "date": date,
-            "numero_decision": numero_decision,
+            "juridiction": metadata[0],
+            "date": metadata[1],
+            "numero_decision": metadata[2],
             "texte": texte_document
         }
+        self.data.append(item)
+
+    def closed(self, reason):
+        logger.warning("\n write_parquet is called \n")
+        logger.warning(reason)
+        df = pd.DataFrame(self.data)
+        logger.warning(df.size)
+        df.to_parquet("legifrance_data.parquet", index=False)
